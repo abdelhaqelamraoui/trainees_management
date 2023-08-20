b87b795b74404e3582b4b3eb00d04da8b657b81f
abdelhaqelamraoui <abdelhaqelamraoui@gmail.com>
2023-08-20 02:13:18 +0100

    added: allowing URLs without .php extension

--- app/views/sidebar.php
+++ app/views/sidebar.php
@@ -3,8 +3,8 @@
 <!-- sidebar -->
 
 <div class="list-group gap-2 mt-3 start-0">
-  <a href="index.php" class="btn btn-outline-light border-0 rounded-0 text-uppercase text-start">Home</a>
-  <a href="list.php" class="btn btn-outline-light border-0 rounded-0 text-uppercase text-start">List</a>
-  <a href="add.php" class="btn btn-outline-light border-0 rounded-0 text-uppercase text-start">Add</a>
-  <a href="search.php" class="btn btn-outline-light border-0 rounded-0 text-uppercase text-start">Search</a>
+  <a href="index" class="btn btn-outline-light border-0 rounded-0 text-uppercase text-start">Home</a>
+  <a href="list" class="btn btn-outline-light border-0 rounded-0 text-uppercase text-start">List</a>
+  <a href="add" class="btn btn-outline-light border-0 rounded-0 text-uppercase text-start">Add</a>
+  <a href="search" class="btn btn-outline-light border-0 rounded-0 text-uppercase text-start">Search</a>
 </div>
